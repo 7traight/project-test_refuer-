e4deae6aae256ea08f4dc751de5cfa2b184264cb
7traight <17Supra17@gmail.com>
2022-04-20 19:30:24 +0300

    AWD

--- my_app.py
+++ my_app.py
@@ -1,10 +1,6 @@
 from PyQt5.QtWidgets import*
 from instr import *
 
-txt_title = 'Здоровье'
-win_x, win_y = 200, 100
-win_width, win_height = 1000, 600
-
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
@@ -21,6 +17,6 @@ class MainWin(QWidget):
     def connects(self): 
         pass
 
-    app = QApplication([])
-    mw = MainWin()
-    app.exec_()
+app = QApplication([])
+mw = MainWin()
+app.exec_()
