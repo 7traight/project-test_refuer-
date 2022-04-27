2d58d7697915abc10a355bfe4b75370d73809076
Danila <103379577+padap2@users.noreply.github.com>
2022-04-27 21:32:22 +0300

    !!!!!!!

--- second_win.py
+++ second_win.py
@@ -10,6 +10,7 @@ class TestWin(QWidget):
         self.initUI()
         self.connects()
         self.show()
+        self.setStyleSheet('background: blue;')
     def set_appear(self): 
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
